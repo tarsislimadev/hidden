a071c0521ca3b1df42a731a1e1090fb8be243f11
Tarsis de Lima <br.tmvdl@gmail.com>
2023-02-17 12:42:54 -0300

    Fixing server.py file

--- src/api/server.py
+++ src/api/server.py
@@ -9,23 +9,35 @@ class Request:
   def __init__(self, client) -> None:
     self.chunk = str(client.recv(1024).decode('ascii'))
 
+  def getLines(self):
+    return self.chunk.splitlines()
+
+  def getFirstLine(self):
+    lines = self.getLines()
+    return lines[0].split(' ')
+
   def getMethod(self):
-    return ''
-  
+    firstLine = self.getFirstLine()
+    return firstLine[0]
+
   def getPath(self):
-    return ''
-  
+    firstLine = self.getFirstLine()
+    return firstLine[1] # FIXME
+
   def getQueries(self):
-    return ''
-  
+    firstLine = self.getFirstLine()
+    return firstLine[1] # FIXME
+
   def getQuery(self, name):
-    return ''
+    return self.getQueries()[name]
 
   def getHeaders(self):
-    return ['']
-  
+    headers = []
+    return headers # FIXME
+
   def getBody(self):
-    return ''
+    body = ''
+    return body # FIXME
 
   def __str__(self):
     return self.chunk
@@ -54,14 +66,42 @@ class Response():
     self.json.message = message
     self.json.body = {}
     return self
+  
+  def getStatusMessage(self, status):
+    if status == config.http.status.OK:
+      return 'OK'
+
+    if status == config.http.status.NOT_FOUND:
+      return 'NOT FOUND'
+
+    return 'ERROR'
+  
+  def getFirstLine(self, status):
+    message = self.getStatusMessage(status)
+    return ' '.join(['HTTP/1.1', status, message])
+
+  def parseHeader(name, value = ''):
+    return ': '.join([name, value])
+  
+  def getBodyString(self):
+    return JSONEncoder().encode(self.json)
 
   def __str__(self):
     res = []
-    res.append('HTTP/1.1 200 OK')
-    res.append('Content-Type: application/json')
+
+    # res.append('HTTP/1.1 200 OK')
+    res.append(self.getFirstLine('200'))
+
+    # res.append('Content-Type: application/json')
+    res.append(self.parseHeader('Content-Type', 'application/json'))
+
     res.append('')
-    res.append(JSONEncoder().encode(self.json))
+
+    # res.append(JSONEncoder().encode(self.json))
+    res.append(self.getBodyString())
+
     res.append('')
+
     return str('\r\n'.join(res))
 
 def listen():
@@ -89,8 +129,7 @@ def run(request):
 
   path = request.getPath()
 
-  if (path == 'sync'):
+  if (path == config.http.path.SYNC):
     return actions.Sync(request, response)
 
   return response
-
