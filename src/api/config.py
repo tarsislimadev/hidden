ccdbd12690f697d8453b5732b752401fd01ea2db
Tarsis de Lima <br.tmvdl@gmail.com>
2023-02-17 12:42:04 -0300

    Fixing config.py file

--- src/api/config.py
+++ src/api/config.py
@@ -5,3 +5,16 @@ data_path=os.getenv('DATA_PATH', '/data')
 host=os.getenv('host', '127.0.0.1')
 
 port=int(os.getenv('PORT', '80'))
+
+http = {
+  'status': {
+    'OK': '200',
+    'CLIENT_ERROR': '400',
+    'FORBIDDEN': '403',
+    'NOT_FOUND': '404',
+    'SERVER_ERROR': '500',
+  },
+  'path': {
+    'SYNC': 'sync'
+  }
+}
