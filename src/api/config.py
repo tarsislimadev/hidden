cd185748cd7daa50c25328942218a6b8f3309b55
Tarsis de Lima <br.tmvdl@gmail.com>
2023-02-18 12:25:21 -0300

    Fixing config.py file

--- src/api/config.py
+++ src/api/config.py
@@ -2,19 +2,6 @@ import os
 
 data_path=os.getenv('DATA_PATH', '/data')
 
-host=os.getenv('host', '127.0.0.1')
+host=os.getenv('HOST', '127.0.0.1')
 
 port=int(os.getenv('PORT', '80'))
-
-http = {
-  'status': {
-    'OK': '200',
-    'CLIENT_ERROR': '400',
-    'FORBIDDEN': '403',
-    'NOT_FOUND': '404',
-    'SERVER_ERROR': '500',
-  },
-  'path': {
-    'SYNC': 'sync'
-  }
-}
