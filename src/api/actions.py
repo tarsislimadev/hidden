92ad2714cc75eb8e7b4abbc549792fe0412bb182
Tarsis de Lima <br.tmvdl@gmail.com>
2023-02-18 12:25:44 -0300

    Fixing actions.py file

--- src/api/actions.py
+++ src/api/actions.py
@@ -1,5 +1,14 @@
-# import config
+import config
+from libs.database.Database import Database
+
+db = Database(config.data_path)
 
 def Sync(req, res):
-  print(req, res)
-  return res
+  body = req.getBody()
+  posts = db.index('posts')
+
+  for post in body['list']:
+    print('post: ', post)
+    posts.new().write('text', post['text'])
+
+  return res.setJSON({ 'list' : posts.list() })
